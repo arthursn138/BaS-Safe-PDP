a1ea7aaacfe12762704ecfadb46b0b9527fb649b
Arthur Nascimento <arthursn.138@gmail.com>
2022-12-07 22:11:49 -0500

    Create BaS_Cartpole_Propagate.py

---
+++ Examples/SPlan/BaS_Cartpole_Propagate.py
@@ -0,0 +1,47 @@
+
+import math
+import numpy as np
+from Plottings import plot_cartpole
+from casadi import *
+import scipy.io as sio
+import matplotlib.pyplot as plt
+
+# Load data
+data = np.load('./Results/BaS_Cartpole_Testing_lim_1.npy', allow_pickle=True).item()
+solved_u = data['solved_controls'].flatten()
+dt = data['params']['dt']
+horizon = data['params']['horizon']
+cart_lim = data['params']['cart_lim']
+
+# Parameters
+mc, mp, l, g = 0.5, 0.5, 1, 9.81
+
+# Initialization
+X0 = [0, 0, 0, 0, 1/cart_lim**2]
+traj = [X0]
+
+# System propagation
+for i in range(len(solved_u)):
+    U = solved_u[i]
+    X = traj[i]
+
+    # Augmented Continuous Dynamics
+    dx = X[2]
+    dq = X[3]
+    ddx = (U + mp * sin(X[1]) * (l * X[3] * X[3] + g * cos(X[1]))) / (
+                mc + mp * sin(X[1]) * sin(X[1]))                                # acceleration of x
+    ddq = (-U * cos(X[1]) - mp * l * X[3] * X[3] * sin(X[1]) * cos(X[1]) - (mc + mp) * g * sin(X[1])) / (
+            l * mc + l * mp * sin(X[1]) * sin(X[1]))                            # acceleration of theta
+    dz = (- (1 / (cart_lim ** 2 - X[0] ** 2)) ** 2) * (-2 * X[0]) * X[2]        # BaS Derivative
+
+    # Euler integration
+    x = X[0] + dt * dx
+    q = X[1] + dt * dq
+    dx = X[2] + dt * ddx
+    dq = X[3] + dt * ddq
+    z = X[4] + dt * dz
+
+    traj += [[x, q, dx, dq, z]]
+
+plot_cartpole.plotcartpole([0, math.pi, 0, 0, 1], np.array(traj), U, [], 1)
+plt.show()
